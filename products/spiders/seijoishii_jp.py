0470d13182a010279c6000ae5af9ba77a1c4dfc8
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-04-06 06:45:58 +0000

    Add seijoishii_jp

--- products/spiders/seijoishii_jp.py
+++ products/spiders/seijoishii_jp.py
@@ -2,6 +2,7 @@ from scrapy.spiders import SitemapSpider
 
 from products.structured_data_spider import StructuredDataSpider
 
+
 class SeijoishiiJPSpider(SitemapSpider, StructuredDataSpider):
     name = "seijoishii_jp"
     allowed_domains = ["seijoishii.com"]
