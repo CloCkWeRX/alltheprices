32fc501f6a5e3a07334bf7ca5196c8368e440dae
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-04-06 16:17:29 +0930

    Add Seijoishii (JP) (#18)
    * Add seijoishii_jp
    
    * Add seijoishii_jp

---
+++ products/spiders/seijoishii_jp.py
@@ -0,0 +1,13 @@
+from scrapy.spiders import SitemapSpider
+
+from products.structured_data_spider import StructuredDataSpider
+
+
+class SeijoishiiJPSpider(SitemapSpider, StructuredDataSpider):
+    name = "seijoishii_jp"
+    allowed_domains = ["seijoishii.com"]
+
+    sitemap_urls = ["https://seijoishii.com/robots.txt"]
+    sitemap_rules = [
+        (r"https://seijoishii.com/products/\d+", "parse_sd"),
+    ]
