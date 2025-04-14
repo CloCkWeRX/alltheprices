f0202df76b6fef4d20042bed29cfee65ea68a8d0
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-04-14 08:21:25 +0000

    Add YaokoNet

---
+++ products/spiders/yaoko_net.py
@@ -0,0 +1,13 @@
+from scrapy.spiders import SitemapSpider
+
+from products.structured_data_spider import StructuredDataSpider
+
+
+class YaokoNetSpider(SitemapSpider, StructuredDataSpider):
+    name = "yaoko_net"
+    allowed_domains = ["yaoko-net.com"]
+
+    sitemap_urls = ["https://www.yaoko-net.com/sitemap.xml"]
+    sitemap_rules = [
+        (r"https://www.yaoko-net.com/product/detail/[\w-]+.html", "parse_sd"),
+    ]
