19ef66445df2bcc978f1f4194570b3650f6faf1e
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-04-14 21:18:10 +0930

    Add Willmart (GE) (#86)
    * Add Willmart (GE)
    
    * Fix prices

---
+++ products/spiders/willmart_ge.py
@@ -0,0 +1,14 @@
+from scrapy.spiders import SitemapSpider
+
+# from products.categories import PaymentMethods, map_payment
+from products.structured_data_spider import StructuredDataSpider
+
+
+class WillmartGESpider(SitemapSpider, StructuredDataSpider):
+    name = "willmart_ge"
+    allowed_domains = ["willmart.ge"]
+
+    sitemap_urls = ["https://willmart.ge/robots.txt"]
+    sitemap_rules = [
+        (r"https://willmart.ge/shop/[\w-]+/[\w-]+", "parse_sd"),
+    ]
