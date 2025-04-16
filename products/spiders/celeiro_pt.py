00339e092aa9f774bae829fd308ae20ed01decc6
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-04-16 00:21:49 +0000

    Add Celerio (PT)

---
+++ products/spiders/celeiro_pt.py
@@ -0,0 +1,18 @@
+from scrapy.spiders import SitemapSpider
+
+from products.structured_data_spider import StructuredDataSpider
+
+
+class CeleiroPTSpider(SitemapSpider, StructuredDataSpider):
+    name = "celeiro_pt"
+    allowed_domains = ["celeiro.pt"]
+    item_attributes = {
+        "extras": {
+            "seller": {"@type": "Organization", "@id": "https://www.wikidata.org/wiki/Q114102189", "name": "Celeiro"}
+        }
+    }
+
+    sitemap_urls = ["https://celeiro.pt/robots.txt"]
+    sitemap_rules = [
+        (r"https://celeiro.pt/[\d]+-[\w-]+", "parse_sd"),
+    ]
