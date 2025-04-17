d48c78d9045709953906c19d573b65b90b5a024d
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-04-17 03:47:57 +0000

    Add prices

--- products/spiders/biocoop_fr.py
+++ products/spiders/biocoop_fr.py
@@ -2,6 +2,9 @@ from scrapy.spiders import SitemapSpider
 
 from products.structured_data_spider import StructuredDataSpider
 
+from scrapy.http import Response
+from products.items import Product
+
 
 class BiocoopFRSpider(SitemapSpider, StructuredDataSpider):
     name = "biocoop_fr"
@@ -20,3 +23,17 @@ class BiocoopFRSpider(SitemapSpider, StructuredDataSpider):
     sitemap_rules = [
         (r"https://www.biocoop.fr/[\w-]+.html", "parse_sd"),
     ]
+
+    # TODO: If this pattern is common, extract to opengraphparser
+    def post_process_item(self, item: Product, response: Response, ld_data: dict, **kwargs):
+
+        price = response.xpath('//meta[@property="product:price:amount"]/@content').get()
+        currency = response.xpath('//meta[@property="product:price:currency"]/@content').get()
+
+        item["offers"] = {
+            "@type": "Offer",
+            "price": price,
+            "priceCurrency": currency
+        }
+
+        yield item
