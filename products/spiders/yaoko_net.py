288c73a79a8dea42d40b4c8aee6c9a9d2c1cf7a5
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-04-17 04:49:13 +0000

    Wikidata

--- products/spiders/yaoko_net.py
+++ products/spiders/yaoko_net.py
@@ -6,6 +6,11 @@ from products.structured_data_spider import StructuredDataSpider
 class YaokoNetSpider(SitemapSpider, StructuredDataSpider):
     name = "yaoko_net"
     allowed_domains = ["yaoko-net.com"]
+    item_attributes = {
+        "extras": {
+            "seller": {"@type": "Organization", "@id": "https://www.wikidata.org/wiki/Q11344967", "name": "Yaoko"}
+        }
+    }
 
     sitemap_urls = ["https://www.yaoko-net.com/sitemap.xml"]
     sitemap_rules = [
