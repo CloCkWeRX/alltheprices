c05ed9a9ad8a5df8f801096c4464f13ac01b7791
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-04-17 14:17:30 +0930

    Update dixy_ru.py

--- products/spiders/dixy_ru.py
+++ products/spiders/dixy_ru.py
@@ -6,7 +6,15 @@ from products.structured_data_spider import StructuredDataSpider
 class DixyRUSpider(SitemapSpider, StructuredDataSpider):
     name = "dixy_ru"
     allowed_domains = ["dixy.ru"]
-
+    item_attributes = {
+        "extras": {
+            "seller": {
+                "@type": "Organization",
+                "@id": "https://www.wikidata.org/wiki/Q4161561", 
+                "name": "Dixy"
+            }
+        }
+    }
     sitemap_urls = ["https://dixy.ru/sitemap.xml"]
     sitemap_rules = [
         (r"https://dixy.ru/catalog/[\w-]+/[\w-]+/[\d]+/", "parse_sd"),
