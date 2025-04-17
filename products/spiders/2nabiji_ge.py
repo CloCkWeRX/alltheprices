fb58964d4b000d29e0196fe6342399c8c3bf71fd
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-04-17 04:24:26 +0000

    Add prices

--- products/spiders/2nabiji_ge.py
+++ products/spiders/2nabiji_ge.py
@@ -1,5 +1,7 @@
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
+from products.items import Product
 from products.structured_data_spider import StructuredDataSpider
 
 
@@ -13,3 +15,13 @@ class NabijiGESpider(SitemapSpider, StructuredDataSpider):
     ]
 
     # TODO: Post process item to get prices
+    # TODO: If this pattern is common, extract to opengraphparser
+    def post_process_item(self, item: Product, response: Response, ld_data: dict, **kwargs):
+
+        # ,"price":10.95,"discountPrice":0
+        price_js = response.xpath("//script[contains(text(), '\"discountPrice\":')]/text()").get(0)
+        price = price_js.split('"price":')[1].split(",")[0]
+
+        item["offers"][0]["price"] = price
+
+        yield item
