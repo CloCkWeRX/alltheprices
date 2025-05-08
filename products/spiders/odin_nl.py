8dbc467a73d1d36be032ee67053691d0a0f1306d
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-05-08 12:15:29 +0000

    Prices and other details

--- products/spiders/odin_nl.py
+++ products/spiders/odin_nl.py
@@ -2,6 +2,10 @@ from scrapy.spiders import SitemapSpider
 
 from products.structured_data_spider import StructuredDataSpider
 
+from scrapy.http import Response
+
+# from products.categories import PaymentMethods, map_payment
+from products.items import Product
 
 class OdinNLSpider(SitemapSpider, StructuredDataSpider):
     name = "odin_nl"
@@ -18,6 +22,71 @@ class OdinNLSpider(SitemapSpider, StructuredDataSpider):
 
     sitemap_urls = ["https://www.odin.nl/robots.txt"]
     sitemap_rules = [
-        # https://www.hmart.com/tempura-udon-cup-noodle-cup-2-6oz-75g--6-cups/p
+        # https://www.odin.nl/producten/alle-producten/-185916-pikante-kip-salade/
         (r"https://www.odin.nl/producten/alle-producten/[\w-]+/", "parse_sd"),
     ]
+
+    def map_properties(self, item: Product, response: Response):
+        article_properties = response.xpath('//div[@class="article-properties"]/div/dl/div')
+        for article_property in article_properties:
+            label = article_property.xpath("dt/text()").get()
+            value = article_property.xpath("dd/text()").get().strip()
+
+            if label == "EAN-code":
+                item["gtin"] = value
+            elif label == "Artikelcode":
+                item["ref"] = value
+            elif label == "Kiloprijs":
+                item["extras"]["price_kg"] = value
+            elif label == "Kwaliteit":
+                # BIO? Any others?
+                if value == "-":
+                    pass
+                else: # BIO, any other labels?
+                    item["extras"]["quality"] = value
+            elif label == "Land van herkomst":
+                item["extras"]["countryOfOrigin"] = value
+            else:
+                item["extras"][label] = value
+
+    def map_prices(self, item: Product, response: Response):
+        item["offers"] = []
+        article_properties = response.xpath('//div[@class="article-info"]/dl/div')
+        for article_property in article_properties:
+            label = article_property.xpath("dt/text()").get().strip()
+            value = article_property.xpath("dd/span/text()").get().strip()
+
+            if label == "Merk":
+                item["brand"] = value
+                pass
+            elif label == "Inhoud":
+                # Weight
+                # 250 GR
+                pass
+            elif label == "Prijs":
+                # Price
+                # € 4,59
+                item["offers"].append({
+                    "price": value.split("€ ")[1].replace(",", "."),
+                    "priceCurrency": "EUR"
+                })
+            elif label == "Ledenprijs":
+                # Member Price
+                # € 3,89
+                item["offers"].append({
+                    "name": "Ledenprijs",
+                    "price": value.split("€ ")[1].replace(",", "."),
+                    "priceCurrency": "EUR"
+                })
+            else:
+                print(label)
+                print(value)
+
+
+        
+    def post_process_item(self, item: Product, response: Response, ld_data: dict, **kwargs):
+        """Override with any post-processing on the item."""
+        self.map_properties(item, response)
+        self.map_prices(item, response)
+
+        yield item
