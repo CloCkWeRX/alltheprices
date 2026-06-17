0d14c5043b58480c0ffce5d10ab4e4d64f19e6f8
Daniel O'Connor <daniel.oconnor@gmail.com>
2026-06-17 23:10:53 +0930

    Update combi_de.py

--- products/spiders/combi_de.py
+++ products/spiders/combi_de.py
@@ -4,36 +4,6 @@ from products.structured_data_spider import StructuredDataSpider
 
 
 class CombiDESpider(SitemapSpider, StructuredDataSpider):
-    """
-    Spider for Combi (Germany).
-    Sample output:
-    {
-        'extras': {
-            'seller': {
-                '@id': 'https://www.wikidata.org/wiki/Q1113618',
-                '@type': 'Organization',
-                'name': 'Combi'
-            }
-        },
-        'image': 'https://d2y99t05v0nd2u.cloudfront.net/products/images/4503060174_4000521579203_01.jpg',
-        'name': 'Dr. Oetker Crème fraîche classic',
-        'offers': [
-            {
-                '@type': 'Offer',
-                'availability': 'https://schema.org/InStock',
-                'itemCondition': 'https://schema.org/NewCondition',
-                'priceSpecification': {
-                    '@type': 'UnitPriceSpecification',
-                    'price': '2.69',
-                    'priceCurrency': 'EUR',
-                    'valueAddedTaxIncluded': True
-                }
-            }
-        ],
-        'ref': '4503060174',
-        'website': 'https://www.combi.de/dr._oetker_cr%c3%a8me_fra%c3%aeche_classic_4503060174.html'
-    }
-    """
 
     name = "combi_de"
     allowed_domains = ["combi.de"]
